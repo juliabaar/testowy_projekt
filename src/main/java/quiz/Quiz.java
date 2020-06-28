619e31d37001b2d6a188b6867aa138ecba6b957a
juliabaar <paterska.j@gmail.com>
2020-06-28 10:33:02 +0200

    Dodanie klasy Quiz

---
+++ src/main/java/quiz/Quiz.java
@@ -0,0 +1,16 @@
+package quiz;
+
+public class Quiz {
+    public static void main(String[] args) {
+       for (int i = 0; i <4; i++){
+           if (i/2 ==0){
+               System.out.println(i);
+           }
+       }
+
+    }
+
+}
+
+
+
